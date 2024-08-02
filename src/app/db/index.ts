0d53a89b2644b69c6543a04fbbc73e81c94cc61c
d.romanenkov <d.romanenkov@msocialproduction.com>
2024-08-02 10:10:40 +0300

    feat: add DB methods for users

--- src/app/db/index.ts
+++ src/app/db/index.ts
@@ -1,5 +1,34 @@
+import { User } from "./types";
+
 export class DB {
-  constructor() {}
+  users: User[];
+  constructor() {
+    this.users = [];
+  }
+
+  async getUser(userName: string) {
+    const user = this.users.find(({ name }) => name === userName);
+    return user || null;
+  }
+
+  async addUser(userName: string) {
+    const newUser: User = {
+      name: userName,
+      index: String(Date.now()) + this.users.length,
+    };
+    this.users.push(newUser);
+    return newUser;
+  }
+
+  async deleteUserByUserName(userName: string) {
+    this.users = this.users.filter(({ name }) => name !== userName);
+    return true;
+  }
+
+  async deleteUserByUserIndex(userIndex: string | number) {
+    this.users = this.users.filter(({ index }) => index !== userIndex);
+    return true;
+  }
 }
 
 export default new DB();
